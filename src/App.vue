cbe4aee45551f5e629d66222313dc6e67b77c06e
tian <1192556828@qq.com>
2022-09-06 11:15:35 +0800

    feat:项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <div>1</div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// git log 查看commit记录
+// git status 查看当前仓库状态 是否修改
+//
+export default {}
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style></style>
